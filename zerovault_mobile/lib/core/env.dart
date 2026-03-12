61b1b01aeadac29c932de2c3364cdb348f13489e
Mohammed Sabithulla Sharieff <sabithullasharieff16@gmail.com>
2026-03-12 20:37:19 +0530

    fix: add Supabase anon key to env.dart default value

--- zerovault_mobile/lib/core/env.dart
+++ zerovault_mobile/lib/core/env.dart
@@ -9,6 +9,6 @@ class Env {
   );
   static const supabaseAnonKey = String.fromEnvironment(
     'SUPABASE_ANON_KEY',
-    defaultValue: '',
+    defaultValue: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdjc2JhenptdGl2aG11aWV0YWpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NjE3MDIsImV4cCI6MjA4ODUzNzcwMn0.g3ciFwNv6ei6BMsyWTCgsJqgQcqKIqaKhqK4GCAL5_E',
   );
 }
